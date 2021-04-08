bc32e15783b338fd07651b18dbd3b8d95595b345
SaPE <33146399+petersasi@users.noreply.github.com>
2021-04-08 12:10:58 +0800

    start in the dir where the script is located
    ...plus add copyright

--- 00-bootstrap.cmd
+++ 00-bootstrap.cmd
@@ -1,3 +1,9 @@
+@pushd "%~dp0"
+
+@rem This script is Copyright
+@rem 2019-2020 Martin Hertz (mhertz-Denmark) user of the Deluge Forum https://forum.deluge-torrent.org/
+@rem 2020-2021 Peter Sasi user of the Deluge Forum https://forum.deluge-torrent.org/
+
 @mkdir 99-Downloads
 @cd 99-Downloads
 @curl -C - -O http://repo.msys2.org/distrib/msys2-x86_64-latest.sfx.exe
@@ -10,3 +16,5 @@ set PATH=%~dp0\msys64\usr\bin;%~dp0\msys64\usr\lib\p7zip;%PATH%
 git clone https://github.com/petersasi/deluge2-win-build 90-gitRepo
 mklink %~dp0\90-gitRepo\install_components.cmd %~dp0\01-install_components.cmd
 rem %~dp0\01-install_components.cmd
+
+@popd
